7e4a238a3136692e8d8984c9ad35028eb1491048
Brad Pitcher <bradpitcher@gmail.com>
2023-10-14 14:50:18 -0600

    gh-pages build

---
+++ docs/precache-manifest.1ca7a9bf36cb1447f0dabed9b91fbaee.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a35ca96b439d4424f553541725f489ae",
+    "url": "./index.html"
+  },
+  {
+    "revision": "a9f31727fbca9aac8e99",
+    "url": "./static/css/main.ac9bbaa0.chunk.css"
+  },
+  {
+    "revision": "894a1e9279b2b8e5a3a7",
+    "url": "./static/js/2.1222a76a.chunk.js"
+  },
+  {
+    "revision": "a9f31727fbca9aac8e99",
+    "url": "./static/js/main.3cf6273a.chunk.js"
+  },
+  {
+    "revision": "77355447c501bfc3172a",
+    "url": "./static/js/runtime-main.7ce483fb.js"
+  }
+]);
